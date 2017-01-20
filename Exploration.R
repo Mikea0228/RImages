6bd5c813ae740027cf04276c99d22802e822e60b
Mikea0228 <michaelbird101@gmail.com>
2017-01-20 17:34:00 +0000

    UK flag now works TODO: make plot non square

--- Exploration.R
+++ Exploration.R
@@ -1,7 +1,7 @@
 library(png)
 
 
-filename <- "G3.png"
+filename <- "uk.png"
 MyImage <- readPNG(paste("TestImages/", filename, sep = ""))
 
 png(
@@ -19,12 +19,15 @@ MyImage.border <- MyImage
 MyImage.border[2:(MyDim[1] - 1), 2:(MyDim[2] - 1), 1:3] <- 0
 
 MyImage.recon <- MyImage.border
-for (i in 2:(MyDim[2] - 1)) {
-  for (j in 2:(MyDim[1] - 1)) {
-    MyImage.recon[i,j,] <- 0.5 * (((MyDim[2]-j) * MyImage.border[i,1,] + j* MyImage.border[i,MyDim[2],]) /MyDim[2]
-                                  +
-                                    ((MyDim[1]-i) * MyImage.border[1,j,] + i* MyImage.border[MyDim[2],j,])/MyDim[1]  
-                                  )
+for (i in 2:(MyDim[1] - 1)) {
+  for (j in 2:(MyDim[2] - 1)) {
+    MyImage.recon[i, j, ] <-
+      0.5 * (((MyDim[2] - j) * MyImage.border[i, 1, ] + j * MyImage.border[i, MyDim[2], ]) /
+               MyDim[2]
+             +
+               ((MyDim[1] - i) * MyImage.border[1, j, ] + i * MyImage.border[MyDim[1], j, ]) /
+               MyDim[1]
+      )
     
   }
 }
@@ -36,4 +39,3 @@ png(
 plot(1:2, type = "n")
 rasterImage(MyImage.recon, 1, 1, 2, 2)
 dev.off()
-
